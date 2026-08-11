a307697804292e6be538b143b711a7db16cddc8e
cardgamenusantara <cardgamenusantara@gmail.com>
2026-08-11 22:57:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const wss = new WebSocketServer({ server });
 // HARUS SAMA PERSIS dengan STICKER_LIST di index.html (client),
 // karena ID inilah yang dipetakan ke file gambar/stiker/<id>.mp4
 // =============================================
-const STICKER_IDS = new Set(['nangis', 'Bagus', 'tertawa', 'tengil']);
+const STICKER_IDS = new Set(['nangis', 'bagus', 'tertawa', 'tengil']);
 
 // =============================================
 // DATA KARTU & PROVINSI
